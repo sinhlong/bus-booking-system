3fa6fa6b978fd53ed02ed352917ded1c20b78a52
longe <long@gmail.com>
2026-08-31 13:44:46 +0700

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/busbooking/bus_booking_api/BusBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.busbooking.bus_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingApiApplication.class, args);
+	}
+
+}
